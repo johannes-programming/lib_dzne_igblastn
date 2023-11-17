0bb3a3a141800dfd8b3309bcc3c7b1e190b416ce
Johannes <johannes-programming@posteo.org>
2023-11-17 08:33:28 +0100

    Version 0.2.7

--- src/lib_dzne_igblastn/_functions.py
+++ src/lib_dzne_igblastn/_functions.py
@@ -1,10 +1,9 @@
-
-
 import contextlib as _contextlib
 import os as _os
 import shutil as _shutil
 import tempfile as _tempfile
 
+import handle_igblastn as _hi
 import lib_dzne_filedata as _fd
 
 import lib_dzne_igblastn._parsers as _prs
@@ -20,20 +19,17 @@ def get_cline(
     germline_db_D,
     germline_db_J,
 ):
-    return [
-        prog,
-        '-auxiliary_data', auxiliary_data,
-        '-germline_db_V', germline_db_V,
-        '-germline_db_D', germline_db_D,
-        '-germline_db_J', germline_db_J,
-        '-domain_system', 'imgt',
-        '-num_alignments_V', '1',
-        '-num_alignments_J', '1',
-        '-num_alignments_D', '1',
-        '-outfmt', '7 std qseq sseq btop',
-        '-query', query, 
-        '-out', out,
-    ]
+    return list(
+        _hi.Cline(
+            cmd=prog,
+            auxiliary_data=auxiliary_data,
+            germline_db_V=germline_db_V,
+            germline_db_D=germline_db_D,
+            germline_db_J=germline_db_J,
+            query=query, 
+            out=out,
+        )
+    )
 
 
 def igdata(IGDATA, *, directory):
